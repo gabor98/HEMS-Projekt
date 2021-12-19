44667ffd4c92e264eb3cd2605ca6b1b01aa9fed0
Gabor Moos <gabor.moos@ion2s.com>
2021-12-19 13:22:43 +0100

    Update guest base layout
    - Remove google font
    - Add title

--- resources/views/layout/guest.blade.php
+++ resources/views/layout/guest.blade.php
@@ -5,11 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-
-        <title>{{ config('APP_NAME') }}</title>
+        <title>HEMS - Kantine</title>
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     </head>
     <body class="antialiased flex flex-col min-h-screen">
         {{-- Navigation --}}
