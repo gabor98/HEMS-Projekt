d663e84dd16d4ec4e9b6ccce141070b93e6ea40d
Gabor Moos <gabor.moos@ion2s.com>
2021-12-19 13:15:22 +0100

    Update navigation
    - Add dynamic cart counter

--- resources/views/application/navigation.blade.php
+++ resources/views/application/navigation.blade.php
@@ -17,15 +17,17 @@
     </div>
 
     {{-- Cart --}}
-    <a href="{{ route('cart.index') }}" class="flex items-center gap-3 text-gray-500 hover:bg-gray-100 hover:text-gray-600 p-2 rounded-md transition-colors ease-linear group">
-        <svg class="h-6 w-6 text-gray-400 group-hover:text-gray-600 transition-colors ease-linear" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-        </svg>
+    @if (!empty(Session::get('cart')))
+        <a href="{{ route('cart.index') }}" class="flex items-center gap-3 text-gray-500 hover:bg-gray-100 hover:text-gray-600 p-2 rounded-md transition-colors ease-linear group">
+            <svg class="h-6 w-6 text-gray-400 group-hover:text-gray-600 transition-colors ease-linear" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
 
-        <span class="font-semibold">3</span>
-    </a>
+            <span class="font-semibold">{{ count(Session::get('cart')) }}</span>
+        </a>
+    @endif
 
-    <div class="flex gap-28 bg-white px-10 py-10 absolute top-28 left-0 z-10 w-full shadow-2xl border-t" x-show="menuDropdown" x-transition @click.away="menuDropdown = false">
+    <div class="flex gap-28 bg-white px-10 py-10 absolute top-28 left-0 z-10 w-full shadow-2xl border-t" style="display: none" x-show="menuDropdown" x-transition @click.away="menuDropdown = false">
         <div>
             <p class="font-semibold mb-4 text-gray-600 tracking-wide">Kategorien</p>
 
